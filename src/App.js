81d4518f26496d21426b350e660bcc7b1e401561
Deebendu Kumar <>
2023-03-25 12:28:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import Header from "./components/Header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header title = "Hello">
+            </Header>
+        </div>
+    );
+}
+
+export default App;
